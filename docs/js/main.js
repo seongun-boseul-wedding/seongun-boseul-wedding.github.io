ca865714aa00ab69999d2dfc7a8d59034f32f632
seongun.choi <seongun.choi@linecorp.com>
2021-06-06 12:34:25 +0900

    (fix-up) add wedding video

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,3 +1,7 @@
+$(window).resize(function(){resizeYoutube();});
+$(function(){resizeYoutube();});
+function resizeYoutube(){ $("iframe").each(function(){ if( /^https?:\/\/www.youtube.com\/embed\//g.test($(this).attr("src")) ){ $(this).css("width","100%"); $(this).css("height",Math.ceil( parseInt($(this).css("width")) * 480 / 854 ) + "px");} }); }
+
 $('.navbar-nav>li>a').on('click', function(){
     $('.navbar-collapse').collapse('hide');
 });
